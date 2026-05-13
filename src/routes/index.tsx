75a8e2cab346eae83ee7db9bfbf0823a7e5b4e1b
marcos roberto <marcosads337@gmail.com>
2026-05-13 13:07:47 -0300

    d

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -216,7 +216,7 @@ function Index() {
             href="https://instagram.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="relative rounded-2xl overflow-hidden block w-full h-72 sm:h-[320px] group"
+            className="relative rounded-2xl overflow-hidden block w-full h-72 sm:h-[320px] group object-cover"
           >
             <img
               src={instagramPreview}
